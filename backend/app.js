e15b33bd5ffbd3805e0cf85ad1e25841177a735f
talhaNisar6 <mohammadtalhanisar666@gmail.com>
2023-01-02 15:50:53 +0500

    add existing files to git

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+const travel = require('./routes/travel')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//middleware
+app.use(express.json());
+
+
+//routes
+app.get('/hello',(req,res)=>{
+res.send('Task Manager App')
+})
+
+//app.use('/api/v1/tasks',travel)
+
+const port = 3000;
+
+const start = async()=>{
+    try {
+        //await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
+
